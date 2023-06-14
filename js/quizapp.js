198324e8b89d5264368161b1812072178785257a
__lesovikova__ <__lesovikova@gmail.com__>
2023-06-14 14:51:44 +0100

    add function for displaying question in the form

--- js/quizapp.js
+++ js/quizapp.js
@@ -1,5 +1,5 @@
 class QuizItem {
-    constructor (question, answer1, answer2, answer3, correctAnswer) {
+    constructor (question, correctAnswer) {
         this.question = question;
         this.answers = ["I don't know", "Yes, you have", "I don't believe you have"];
         this.correctAnswer = correctAnswer;
@@ -12,4 +12,34 @@ const question3 = new QuizItem("Have I met the ringbearer on my journeys?", 2);
 const question4 = new QuizItem('Have I been trained as a hunter since I was a child?', 3);
 const question5 = new QuizItem('Have I gone through the Paths of the Dead?', 1);
 
-console.log(question1);
+const quizArray = [question1, question2, question3, question4, question5];
+
+console.log(quizArray);
+
+
+const quizContainer = document.querySelector('.quiz__container');
+
+function displayQuestion(arr, i) {    
+    const p = document.createElement('p');
+    p.textContent = arr[i].question;
+
+    const answer1 = document.createElement('LABEL');
+    answer1.textContent = arr[i].answer1;
+    quizContainer.append(p);
+};
+
+displayQuestion(quizArray, 2);
+
+function displayAnswers(arr, container) {
+    arr.forEach((item) => {
+        const answer = document.createElement('LABEL');
+        const inputItem = document.createElement('INPUT');
+        inputItem.setAttribute('type', 'radio');
+        answer.textContent = item;
+        console.log(inputItem);
+        container.append(inputItem, answer);
+        console.log(container);
+    });
+}
+
+displayAnswers(quizArray[0].answers, quizContainer);
